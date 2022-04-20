21b73b3eb41c6d9aae044991d99f6b1ec0d81795
Jhodel Cailan <jhodel.cailan@gmail.com>
2022-04-20 11:48:53 +0800

    Added mapbox

--- app/map/src/App.js
+++ app/map/src/App.js
@@ -1,31 +1,51 @@
 import {
-  FlexBox,
-  FlexBoxAlignItems,
-  FlexBoxDirection,
-  FlexBoxJustifyContent,
-  Link,
-  LinkDesign,
-  ShellBar,
-  ThemeProvider
+	FlexBox,
+	FlexBoxAlignItems,
+	FlexBoxDirection,
+	FlexBoxJustifyContent,
+	ShellBar,
+	Card,
+	ThemeProvider
 } from '@ui5/webcomponents-react';
 import './App.css';
 
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFydGluc3RlbnppZyIsImEiOiJjazV1amZpdGwwZG92M2xucDhvbWoxMTB2In0.JWYYOv7JzUpGA51DQLQK-A';
+
 function App() {
-  return (
-    <ThemeProvider>
-      <ShellBar primaryTitle="UI5 Web Components for React Template" />
-      <FlexBox
-        style={{ width: '100%', height: '100vh' }}
-        direction={FlexBoxDirection.Column}
-        justifyContent={FlexBoxJustifyContent.Center}
-        alignItems={FlexBoxAlignItems.Center}
-      >
-        <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
-          Getting Started with UI5 Web Component for React
-        </Link>
-      </FlexBox>
-    </ThemeProvider>
-  );
+	const mapContainer = useRef(null);
+	const map = useRef(null);
+	const [lng, setLng] = useState(-70.9);
+	const [lat, setLat] = useState(42.35);
+	const [zoom, setZoom] = useState(9);
+
+	useEffect(() => {
+		if (map.current) return; // initialize map only once
+		map.current = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [lng, lat],
+			zoom: zoom
+		});
+	});
+
+	return (
+		<ThemeProvider>
+			<ShellBar primaryTitle="UI5 Web Components for React Template" />
+			<FlexBox
+				style={{ width: '100%', height: '100%' }}
+				direction={FlexBoxDirection.Column}
+				justifyContent={FlexBoxJustifyContent.Center}
+				alignItems={FlexBoxAlignItems.Center}
+			>
+				<Card>
+					<div ref={mapContainer} className="map-container" />
+				</Card>
+			</FlexBox>
+		</ThemeProvider>
+	);
 }
 
 export default App;
