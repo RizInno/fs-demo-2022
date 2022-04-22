aa644f6afc038113590581cd7e9874d91072de63
Jhodel Cailan <jhodel.cailan@gmail.com>
2022-04-22 08:55:56 +0800

    Fixed issue on closed websocket

--- app/map/src/WebSocketProvider.js
+++ app/map/src/WebSocketProvider.js
@@ -6,23 +6,40 @@ const url = `${protocol}://${host}/ws`;
 export default class WebSocketProvider {
 
 	constructor() {
-		this.ws = new WebSocket(url);
+		this.ws = this.createWebSocket();
+	}
+
+	createWebSocket() {
+		const ws = new WebSocket(url);
 
 		// Event handler for on open
-		this.ws.onopen = function(event) {
+		ws.onopen = event => {
 			console.log('WebSocket connection open', event.data);
 		};
 
 		// event handler for when the connection is closed
-		this.ws.onclose = function(event) {
+		ws.onclose = event => {
 			console.log('WebSocket connection closed', event.data);
+			// web socket gets automatically closed after few minutes,
+			// therefore, recreate it whe it happens
+			this.ws = this.createWebSocket();
 		};
+
+		if (this.messageReceived) {
+			// reattach onmessage listener
+			ws.onmessage = event => {
+				this.messageReceived(JSON.parse(event.data));
+			};
+		}
+
+		return ws;
 	}
 
 	attachEvent(name, handler) {
 		switch (name) {
 			case "messageReceived":
-				this.ws.onmessage = function(event) {
+				this.messageReceived = handler;
+				this.ws.onmessage = event => {
 					handler(JSON.parse(event.data));
 				};
 				break;
